8f71b026025b8718569a4cf8a615d019f13aca1a
Juan Juan <juannvictor1998@gmail.com>
2020-04-01 13:14:41 -0300

    Add files via upload

---
+++ matrizes.c
@@ -0,0 +1,26 @@
+int main(int argc, char *argv[])
+{ int linha;
+  int coluna;
+  int i;
+  int somaprod;
+  int mat1[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+  int mat2[3][3]={{1,0,0},{0,1,0},{0,0,1}};
+  int mat3[3][3];
+  int M1L=3, M1C=3, M2L=3, M2C=3;
+  for(linha=0; linha<M1L; linha++) 
+    for(coluna=0; coluna<M2C; coluna++){ 
+      somaprod=0; 
+      for(i=0; i<M1L; i++) somaprod+=mat1[linha][i]*mat2[i][coluna]; 
+      mat3[linha][coluna]=somaprod; 
+    } 
+  // 
+  //imprime mat3 
+  // 
+  for(linha=0; linha<M1L; linha++){ 
+    for(coluna=0; coluna<M2C; coluna++) 
+      printf("%d ", mat3[linha][coluna]); 
+    printf("\n"); 
+  }
+  system("PAUSE");      
+  return 0;
+}
